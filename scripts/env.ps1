735145dbd019bc0f5bdef48f5a457ce4a2666dbf
Justin Luther <8236331+jerky676@users.noreply.github.com>
2020-01-14 12:52:30 -0600

    Adding add-gitaccessotken to set up git

--- scripts/env.ps1
+++ scripts/env.ps1
@@ -3,6 +3,14 @@ $tests="$parent\tests"
 $src="$parent\src"
 $publish="$parent\publish"
 
+function Add-GitAccessToken(){
+    git config --global credential.helper store
+    Add-Content "$HOME\.git-credentials" "https://$($env:GitHubAccessToken):x-oauth-basic@github.com`n"
+    git config --global user.email "$env:GitHubEmail"
+    git config --global user.name "$env:GitHubUserName"
+}
+
+
 function Invoke-GitVersions(){
     GitVersion.exe /output buildserver
 }
